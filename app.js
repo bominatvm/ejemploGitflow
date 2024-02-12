b80db68ae8f3b34278743403f38f87cb2c7def0d
lealivan009 <lealivan009@gmail.com>
2024-02-12 16:04:12 -0300

    Pipeline con cierre de puerto

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-// app.js
+// Importar el módulo express
 const express = require('express');
-const app = express();
-const port = 3000;
 
+// Crear una instancia de la aplicación Express
+const app = express();
+
+// Ruta principal que responde con Hola, mundo!
 app.get('/', (req, res) => {
-    res.send('¡Hola Mundo!');
+  res.send('Hola, mundo!');
 });
 
-app.listen(port, () => {
-    console.log(`La aplicación está escuchando en http://localhost:${port}`);
+// Puerto en el que la aplicación escuchará las solicitudes
+const puerto = 3000;
+
+// Iniciar el servidor y escuchar en el puerto especificado
+const server = app.listen(puerto, () => {
+  console.log();
+  
+  // Cerrar el servidor después de 5 segundos
+  setTimeout(() => {
+    console.log('Cerrando el servidor después de 5 segundos');
+    server.close();
+  }, 5000);
 });
