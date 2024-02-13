50a69e0c54c0a38c548f7e8df9c1737aa6294fe0
Nhay2008 <159199331+Nhay2008@users.noreply.github.com>
2024-02-13 23:27:41 +0100

    Create AppHola.js

---
+++ .github/workflows/AppHola.js
@@ -0,0 +1,31 @@
+
+
+name: Node.JS CI
+
+on:
+
+  push:
+    branches: [ "Maicol" ]
+
+jobs:
+  
+  build:
+
+    runs-on: windows-latest
+
+    steps:
+      
+      - uses: actions/checkout@v3
+        
+      - name: Use Node.js
+        uses: actions/setup-node@v3
+
+        with:
+          node-version: '14.x'
+
+      - name: Dependencias
+        run: npm install express
+
+      - name: node apphola.js 
+        run: node apphola.js
+          
