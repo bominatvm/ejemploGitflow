61db9f593837c41cd0a453ca321c276bd2dba035
Martín <martinchogimenez@gmail.com>
2024-02-14 08:01:02 -0300

    Modificando hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -1,2 +1,15 @@
-// index.js
-console.log('Hello, world!');
+// Importa Express
+const express = require('express');
+
+// Crea una instancia de Express
+const app = express();
+
+// Define una ruta de ejemplo
+app.get('/', (req, res) => {
+    res.send('¡Hola, esto es una creacion de app en nodejs');
+});
+
+// Inicia el servidor en el puerto 3000
+app.listen(3000, () => {
+    console.log('Servidor iniciado en http://localhost:3000');
+});
